fa302b374dcabc911286b3c352c20b4b33c8a966
shanokthethird <xxshanok3xx@gmail.com>
2026-06-17 04:20:45 +0000

    first commit

---
+++ main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    char world[] = "world";
+    char grade = 'A';
+
+    int age = 54;
+    double smh = 34.5;
+
+
+    printf("Hello, %s!\n", world);
+    return 0;
+}
